5e9f4999a01ef2c9223fc4dfa9bafe9301f65740
e-aub <eaub@proton.me>
2024-03-30 01:22:40 +0000

    fix

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crudapi
+
+go 1.21.7
+
+require github.com/gorilla/mux v1.8.1
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	movies = append(movies, Movie{ID: "001", Isbn: "0115", Title: "Interstellar", Director: &Director{
+		FirstName: "Christopher",
+		LastName:  "Nolan",
+	}})
+	movies = append(movies, Movie{ID: "002", Isbn: "011543", Title: "Oppenheimer", Director: &Director{
+		FirstName: "john",
+		LastName:  "doo",
+	}})
+	r := mux.NewRouter()
+	//defining routes
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
